725e32e18e8d6890e4447269777eff3e59b21d2a
Eva Lieu <itsevalieu@gmail.com>
2017-03-03 23:28:02 -0800

    Add images and icons directory

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,15 +5,15 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header/>
-        {this.props.children}
-        <Footer/>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header/>
+				{this.props.children}
+				<Footer/>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, browserHistory } from "react-router";
 import makeRoutes from "./containers/routes";
 
-const APP_ENTRY = <Router history={ browserHistory }>
-	{makeRoutes()}
-	</Router>;
+const APP_ENTRY = <Router history={ browserHistory }>{makeRoutes()}</Router>;
 
 ReactDOM.render(
   APP_ENTRY,
